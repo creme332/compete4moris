dfc8a0b20a8037c157e69eb324158cb8e7997f35
creme332 <65414576+creme332@users.noreply.github.com>
2022-08-03 17:46:04 +0400

    Add feature to show currently active tag

--- script.js
+++ script.js
@@ -11,7 +11,15 @@ tags.forEach(tag=>tag.addEventListener("click", function(event){
     cards.forEach(card=>{
         card.style.display = "block";
     });
+    //remove all borders from tags
+    tags.forEach(t=>{
+        t.style.outline = "none";
+    });
+    // add border to clicked tag
     const clickedTag = event.target;
+    clickedTag.style.outline ="1px solid";
+    if(clickedTag.textContent == "All")return;
+
     // console.log(clickedTag.textContent);
 
     //check if clicked tag is present in each card.
