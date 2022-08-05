f3448014859543bd2faeb1d49f8213d4cd106472
creme332 <65414576+creme332@users.noreply.github.com>
2022-08-05 13:00:02 +0400

    Change behaviour of tag filtering
    All selected tags are now OR'ed instead of AND'ed.

--- script.js
+++ script.js
@@ -1,50 +1,77 @@
 //tags in header
 const tags =  document.querySelector(".card-tags").querySelectorAll("li");
-
+const Alltag =  document.querySelector(".card-tags").querySelector("li");
 const cards =  document.querySelectorAll(".card");
+const borderStyle = "solid 1px";
+let activeTags = new Set();
+activeTags.add("All");
 
+//Filter cards displayed by tag
 tags.forEach(tag=>tag.addEventListener("click", function(event){
-    //display all cards first
+    const clickedTag = event.target;
+
+    //display all cards 
     cards.forEach(card=>{
         card.style.display = "block";
     });
-    //remove all borders from tags
-    tags.forEach(t=>{
-        t.style.outline = "none";
-    });
-    // add border to clicked tag
-    const clickedTag = event.target;
-    clickedTag.style.outline ="1px solid";
-    if(clickedTag.textContent == "All")return;
+    
+    if(clickedTag.textContent == "All"){
+        //remove all active tags
+        activeTags.clear()
+        tags.forEach(t=>{t.style.outline = "none";});
 
-    // console.log(clickedTag.textContent);
+        //style only All tag
+        activeTags.add("All");
+        Alltag.style.outline = borderStyle;
+        return;
+    }
 
-    //check if clicked tag is present in each card.
-    // if not, hide card
-    cards.forEach(card=>{
-        let currentTags = card.querySelector(".card-tags").querySelectorAll("li");
-        let validCard  = false;
-        currentTags.forEach(t=>{
-            if(t.textContent == clickedTag.textContent){
-                validCard = true;
-                return;
-            }
-        });
-        if(!validCard){
-            card.style.display = "none";
+    //remove All from activeTags
+    activeTags.delete("All");
+    Alltag.style.outline ="none";
+
+    if(clickedTag.style.outline == borderStyle){ //tag already active
+        //unselect tag
+        clickedTag.style.outline ="none";
+        activeTags.delete(clickedTag.textContent);
+
+        if(activeTags.size == 0){ // no tags selected
+            activeTags.add("All"); //display all
+            Alltag.style.outline = borderStyle;
         }
-        // console.log(currentTags);
-    });
+    }else{ //tag inactive
+        //select tag
+        clickedTag.style.outline = borderStyle;
+        activeTags.add(clickedTag.textContent);
+    }
+    
+    //update card list shown
+    if(!activeTags.has("All")){
+        //for each displayed card,
+        //check if card tags is present in activeTags.
+        // if not, hide card
+        cards.forEach(card=>{
+            let currentTags = card.querySelector(".card-tags").querySelectorAll("li");
+            let validCard  = false;
+            currentTags.forEach(t=>{
+                if(activeTags.has(t.textContent)){
+                    validCard = true;
+                    return;
+                }
+            });
+            if(!validCard){
+                card.style.display = "none";
+            }
+            // console.log(currentTags);
+        });
+    }
+
+
 
 }));
 
+//Flip card when clicked
 cards.forEach(card=>{
-
-    console.log("front :", card.offsetHeight.toString() +"px");
-    console.log("back", card.querySelector(".card-back").style.height);
-    
-    
-    //add eventlistener to rotate card when clicked
     card.addEventListener("click",function(){
         const inner = card.querySelector(".card-inner");
         if(inner.classList.contains("rotate-inner")){
